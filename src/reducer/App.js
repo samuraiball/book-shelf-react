f0c07556dd6db5462d5a6710ee33dc337b64d68a
hirooka <yuy.hirohiro@gmail.com>
2019-01-28 20:35:46 +0900

    add 'react-redux' adn 'react-router-dpm'

---
+++ src/reducer/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+
+const App = () =>  (
+    <BrowserRouter>
+        <div>
+            <Route exact pass='/login' component={}/>
+            <Route exact pass='/login' component={}/>
+        </div>
+    </BrowserRouter>
+)
